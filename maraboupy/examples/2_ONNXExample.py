25cca39bcbb8a2366a46364ac9e31e225829b457
kjulian3 <kjulian3@stanford.edu>
2020-07-07 14:45:30 -0700

    Changes for building documentation (#319)
    * Doc files
    
    * Update headings for readme

--- maraboupy/examples/2_ONNXExample.py
+++ maraboupy/examples/2_ONNXExample.py
@@ -1,107 +1,130 @@
 '''
-/* *******************                                                        */
-/*! \file MarabouNetworkTFExample.py
- ** \verbatim
- ** Top contributors (to current version):
- **   Kyle Julian
- ** This file is part of the Marabou project.
- ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved. See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **
- ** \brief [[ Add one-line brief description here ]]
- **
- ** [[ Add lengthier description here ]]
- **/
+ONNX Example
+====================
+
+Top contributors (to current version):
+  - Kyle Julian
+  
+This file is part of the Marabou project.
+Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+in the top-level source directory) and their institutional affiliations.
+All rights reserved. See the file COPYING in the top-level source
+directory for licensing information.
 '''
 
 from maraboupy import Marabou
-from maraboupy import MarabouCore
 import numpy as np
 
+# %%
 # Set the Marabou option to restrict printing
-options = MarabouCore.Options()
-options._verbosity = 0
+options = Marabou.createOptions(verbosity = 0)
 
-### FULLY CONNECTED NETWORK EXAMPLE ###
-# Network corresponds to inputs x0, x1
-# Outputs: y0 = |x0| + |x1|, y1 = x0^2 + x1^2
+# %%
+# Fully-connected network example
+# -------------------------------
+#
+# This network has inputs x0, x1, and was trained to create outputs that approximate
+# y0 = abs(x0) + abs(x1), y1 = x0^2 + x1^2
 print("Fully Connected Network Example")
-filename = './networks/graph_test_medium.onnx'
+filename = "../../resources/onnx/fc1.onnx"
 network = Marabou.read_onnx(filename)
 
-## Or, you can specify the operation names of the input and output operations
-## By default chooses the only placeholder as input, last op as output
-#inputName = 'Placeholder:0'
-#outputName = 'y_out:0'
-#network = Marabou.read_onnx(filename=filename, inputNames=[inputName], outputName = outputName)
+# %%
+# Or, you can specify the operation names of the input and output operations.
+# The default chooses the placeholder operations as inputs and the last operation as output
+inputName = 'Placeholder:0'
+outputName = 'y_out:0'
+network = Marabou.read_onnx(filename=filename, inputNames=[inputName], outputName = outputName)
 
+# %%
 # Get the input and output variable numbers; [0] since first dimension is batch size
 inputVars = network.inputVars[0][0]
 outputVars = network.outputVars[0]
 
+# %%
 # Set input bounds
 network.setLowerBound(inputVars[0],-10.0)
 network.setUpperBound(inputVars[0], 10.0)
 network.setLowerBound(inputVars[1],-10.0)
 network.setUpperBound(inputVars[1], 10.0)
 
+# %%
 # Set output bounds
 network.setLowerBound(outputVars[1], 194.0)
 network.setUpperBound(outputVars[1], 210.0)
 
+# %%
 # Call to Marabou solver
 vals, stats = network.solve(options = options)
 
 
-### CONVOLUTIONAL NETWORK EXAMPLE ###
+# %%
+# Convolutional neural network example
+# ------------------------------------
+#
 # Network maps 8x16 grayscale images two values
 print("\nConvolutional Network Example")
-filename = './networks/AutoTaxi.onnx'
+filename = '../../resources/onnx/KJ_TinyTaxiNet.onnx'
 network = Marabou.read_onnx(filename)
 
+# %%
 # Get the input and output variable numbers; [0] since first dimension is batch size
 inputVars = network.inputVars[0][0]
 outputVars = network.outputVars[0]
 
+# %%
+# Setup a local robustness query
 delta = 0.03
 for h in range(inputVars.shape[0]):
     for w in range(inputVars.shape[1]):
         network.setLowerBound(inputVars[h][w][0], 0.5-delta)
         network.setUpperBound(inputVars[h][w][0], 0.5+delta)
 
+# %%
 # Set output bounds
 network.setLowerBound(outputVars[0], 6.0)
 
+# %%
 # Call to Marabou solver (should be SAT)
 print("Check query with less restrictive output constraint (Should be SAT)")
 vals, stats = network.solve(options = options)
+assert len(vals) > 0
 
-
+# %%
 # Set more restrictive output bounds
 network.setLowerBound(outputVars[0], 7.0)
 
+# %%
 # Call to Marabou solver (should be UNSAT)
 print("Check query with more restrictive output constraint (Should be UNSAT)")
 vals, stats = network.solve(options = options)
+assert len(vals) == 0
 
 
-### CONVOLUTIONAL NETWORK WITH MAX-POOL EXAMPLE ###
+# %%
+# Convolutional network with max-pool example
+# -------------------------------------------
 print("\nConvolutional Network with Max Pool Example")
-filename = './networks/graph_test_cnn.onnx'
+filename = '../../resources/onnx/conv_mp1.onnx'
 network = Marabou.read_onnx(filename)
 
+# %%
 # Get the input and output variable numbers; [0] since first dimension is batch size
 inputVars = network.inputVars[0]
 outputVars = network.outputVars
 
-marabouEval = network.evaluateWithMarabou([np.ones(inputVars.shape)], options = options)
-onnxEval = network.evaluateWithoutMarabou([np.ones(inputVars.shape)])
+# %% 
+# Test Marabou equations against onnxruntime at an example input point
+inputPoint = np.ones(inputVars.shape)
+marabouEval = network.evaluateWithMarabou([inputPoint], options = options)
+onnxEval = network.evaluateWithoutMarabou([inputPoint])
 
+# %%
+# The two evaluations should produce the same result
 print("Marabou Evaluation:")
 print(marabouEval)
 print("\nONNX Evaluation:")
 print(onnxEval)
 print("\nDifference:")
 print(onnxEval - marabouEval)
+assert max(abs(onnxEval - marabouEval)) < 1e-6
